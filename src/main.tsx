b7429fc113b224775e64ed3a497fc165954db9ae
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-16 10:10:33 +0700

    fix: update BrowserRouter basename from '/temu-lokal/' to '/temulokal/'

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/temu-lokal/'>
+    <BrowserRouter basename='/temulokal/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
