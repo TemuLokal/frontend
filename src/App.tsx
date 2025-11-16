029157f54a05699faba64e01e4375b3f52db4dcb
Kaffahh <kaffah505@gmail.com>
2025-11-16 10:35:05 +0700

    Modification for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import Navbar from './components/Navbar'
-import Home from './components/Home';
-import Chart from './components/Chart';
-import UMKM from './components/UMKM';
-import Article from './components/Article';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-export default function App() {
+export default function App({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <div className="min-h-screen">
-        <Home />
-        <section className="-mt-20 md:-mt-24 lg:-mt-32 relative z-10">
-          <Chart widthPercent={0.95} layout="row" />
-        </section>
-        <UMKM />
-        <Article />
+        {children}
       </div>
       <Footer />
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,58 +1,103 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+// main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from "./App";
-import DetailUMKM from "./components/DetailUMKM";
-import DetailArticle from "./components/DetailArticle";
+import Home from "./components/Home";
+import Chart from "./components/Chart";
 import UMKM from "./components/UMKM";
 import Article from "./components/Article";
+import DetailUMKM from "./components/DetailUMKM";
+import DetailArticle from "./components/DetailArticle";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 
-import "./index.css";
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <App>
+        <>
+          <Home />
+          <section className="-mt-20 md:-mt-24 lg:-mt-32 relative z-10">
+            <Chart widthPercent={0.95} layout="row" />
+          </section>
+          <UMKM />
+          <Article />
+        </>
+      </App>
+    ),
   },
+
   {
     path: "/umkm",
-    element: <UMKM />,
+    element: (
+      <App>
+        <UMKM />
+      </App>
+    ),
   },
+
   {
     path: "/umkm/:id",
-    element: <DetailUMKM />,
+    element: (
+      <App>
+        <DetailUMKM />
+      </App>
+    ),
   },
+
   {
     path: "/article",
-    element: <Article />,
+    element: (
+      <App>
+        <Article />
+      </App>
+    ),
   },
+
   {
     path: "/article/:id",
-    element: <DetailArticle />,
+    element: (
+      <App>
+        <DetailArticle />
+      </App>
+    ),
   },
+
   {
     path: "/register",
-    element: <Register />,
+    element: (
+      <App>
+        <Register />
+      </App>
+    ),
   },
+
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <App>
+        <Login />
+      </App>
+    ),
   },
+
   {
     path: "*",
-    element: <NotFound />,
+    element: (
+      <App>
+        <NotFound />
+      </App>
+    ),
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
