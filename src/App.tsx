ca143ca6aba9741b1cb0b2060472d79512385ecb
Kaffahh <kaffah505@gmail.com>
2025-11-16 10:08:14 +0700

    Change base on vite.config.ts and adding vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './components/Home';
 import Chart from './components/Chart';
 import UMKM from './components/UMKM';
 import Article from './components/Article';
 import Footer from './components/Footer';
-import DetailUMKM from './components/DetailUMKM';
-import DetailArticle from './components/DetailArticle';
-import NotFound from './components/NotFound';
-import Register from './components/Register';
-import Login from './components/Login';
 
-function App() {
+export default function App() {
   return (
     <>
       <Navbar />
       <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Home />
-              <section className="-mt-20 md:-mt-24 lg:-mt-32 relative z-10">
-                <Chart widthPercent={0.95} layout="row" />
-              </section>
-              <UMKM />
-              <Article />
-            </>
-          } />
-          <Route path="/umkm/:id" element={<DetailUMKM />} />
-          <Route path="/umkm" element={<UMKM />} />
-          <Route path="/article/:id" element={<DetailArticle />} />
-          <Route path="/article" element={<Article />} />
-          <Route path="/register" element={<Register /> } />
-          <Route path="/login" element={<Login /> } />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <Home />
+        <section className="-mt-20 md:-mt-24 lg:-mt-32 relative z-10">
+          <Chart widthPercent={0.95} layout="row" />
+        </section>
+        <UMKM />
+        <Article />
       </div>
+      <Footer />
     </>
-  )
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,58 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import App from "./App";
+import DetailUMKM from "./components/DetailUMKM";
+import DetailArticle from "./components/DetailArticle";
+import UMKM from "./components/UMKM";
+import Article from "./components/Article";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/umkm",
+    element: <UMKM />,
+  },
+  {
+    path: "/umkm/:id",
+    element: <DetailUMKM />,
+  },
+  {
+    path: "/article",
+    element: <Article />,
+  },
+  {
+    path: "/article/:id",
+    element: <DetailArticle />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
